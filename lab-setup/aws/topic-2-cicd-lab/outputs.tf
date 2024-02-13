0dc6d568cc9efd7b480d9e9fdd181654af2f1f22
Eric Kim <44147889+taehyunnkim@users.noreply.github.com>
2024-02-12 16:56:18 -0800

    lab: add simple wg peer generation script

--- lab-setup/aws/topic-2-cicd-lab/outputs.tf
+++ lab-setup/aws/topic-2-cicd-lab/outputs.tf
@@ -13,3 +13,8 @@ output "please_note" {
 output "SSH" {
   value = "ssh -i ${var.ssh_key_name}.pem ubuntu@${aws_instance.topic-2-lab.public_ip}"
 }
+
+output "ec2_public_ip" {
+  value = aws_instance.topic-2-lab.public_ip
+  description = "The public IP address of the EC2 instance."
+}
