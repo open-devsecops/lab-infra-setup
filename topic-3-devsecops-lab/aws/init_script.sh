fd18eff839a813d3c577444bc1f8cec151cab095
Eric Kim <44147889+taehyunnkim@users.noreply.github.com>
2024-03-05 00:25:15 -0800

    uncomment for full setup

--- topic-3-devsecops-lab/aws/init_script.sh
+++ topic-3-devsecops-lab/aws/init_script.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 set -e
 
-# # Update node.js
-# sudo npm install n -g
-# sudo n latest
+# Update node.js
+sudo npm install n -g
+sudo n latest
 
-# # Run vpn-config-generator
-# npm install pm2 -g
-# git clone https://github.com/open-devsecops/vpn-config-generator.git 
-# cd vpn-config-generator
-# npm install
-# pm2 start index.js
-# cd ..
+# Run vpn-config-generator
+npm install pm2 -g
+git clone https://github.com/open-devsecops/vpn-config-generator.git 
+cd vpn-config-generator
+npm install
+pm2 start index.js
+cd ..
 
-# # Add Docker's official GPG key:
-# sudo apt-get update
-# sudo apt-get -y install ca-certificates curl
-# sudo install -m 0755 -d /etc/apt/keyrings
-# sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-# sudo chmod a+r /etc/apt/keyrings/docker.asc
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get -y install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# # Add the repository to Apt sources:
-# echo \
-#   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-#   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-#   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-# sudo apt-get update
-# sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-# sudo docker compose -f /home/ubuntu/open-devsecops/docker-compose.yml up -d 
+sudo docker compose -f /home/ubuntu/open-devsecops/docker-compose.yml up -d 
 
 # Intall Postgres
 sudo sh -c 'echo "deb https://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
@@ -87,7 +87,7 @@ sudo systemctl enable sonarqube
 sudo systemctl start sonarqube
 
 # Install Trivy
-sudo apt-get install wget apt-transport-https gnupg lsb-release
+sudo apt-get install -y wget apt-transport-https gnupg lsb-release
 wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
 echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
 sudo apt-get update
